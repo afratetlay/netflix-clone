fedf8e9f5e8827d7b515469086a39f5cb9322b95
Afra Anwar <afraanwar@Afras-Air.local>
2022-07-07 15:01:31 +0200

    Working on the styling for the Netflix Originals

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React from "react";
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey Clever Programmer</h1>
+      {/** NavBar */}
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true} // The CSS is targeting the Netflix Originals because of this isLargeRow
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorroMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,19 +1,46 @@
 import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
 
-function Row({ title }) {
+// The base URL we need for the images
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
   //Store some info - a way to write variables in react
   const [movies, setMovies] = useState([]);
 
   // A snippet of code which runs based on a specific condition/variable
   useEffect(() => {
     // Pull in the information when this code loads
+
+    // This is how you write async into useEffect, this will fetch the data and wait.
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results); // We are going to request the data .results
+      return request;
+    }
+    fetchData();
     // if we leave the [] blank, run once when the row loads, and don't run it again
-  }, []);
+  }, [fetchUrl]);
+  // console.log(movies);
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
 
+      <div className="row__posters">
+        {/**  Several Row poster */}
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`} // Get the larger styles if it's a larger row
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path // if you are using a LargeRow then use poster_path otherwise use the movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
       {/* Container -> With posters inside */}
     </div>
   );
